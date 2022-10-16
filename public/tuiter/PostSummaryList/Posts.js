cc70baf2e26580d2faac13b24c787d573412e372
agryash <yash.agrawal2@learner.manipal.edu>
2022-10-16 19:33:29 -0400

    a5 JavaScript sp22

---
+++ public/tuiter/PostSummaryList/Posts.js
@@ -0,0 +1,33 @@
+export default [
+    {
+        topic: 'Global Warming',
+        userName: 'earth',
+        time: '12h',
+        title: 'Incorrect Garbage Disposal the primary reason for icecaps melting',
+        image: 'garbage.jpg'
+    },
+    {
+        topic: 'World',
+        userName: 'paris',
+        time: '23h',
+        title: 'Climate Summit in Paris',
+        image: 'paris.jpg',
+        tweets: '123K',
+    },
+    {
+        topic: 'Business',
+        userName: 'bloomberg',
+        title: 'Bloomberg remains the only financial institution to announce recession.',
+        time: '2d',
+        image: 'bloomberg.jpeg',
+        tweets: '122K',
+    },
+    {
+        topic: 'News',
+        userName: 'The New York Times',
+        title: 'CNN\'s Cuomo conundrum: A star anchor with a brother in trouble',
+        time: '1m',
+        image: 'mass.jpeg',
+        tweets: '120K',
+    },];
+ 
---
+++ public/tuiter/WhoToFollowList/Who.js
@@ -0,0 +1,12 @@
+export default [
+    {   avatarIcon: 'akamai.webp',
+      userName: 'Akamai', handle: 'akamai', },
+    {   avatarIcon: 'hilabs.jpeg',
+      userName: 'HiLabs',
+      handle: 'hilabs', },
+    {   avatarIcon: 'bill.jpg',
+      userName: 'Bill Gates', handle: 'gates', },
+    {   avatarIcon: 'neu.jpg',
+      userName: 'Northeastern University', handle: 'neu', }, ];
+   
+   
